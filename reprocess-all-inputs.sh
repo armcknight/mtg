4069125bd825ba7949f472c5ea150460890fecb6
Andrew McKnight <git@mcknight.rocks>
2024-01-14 01:53:51 -0900

    fix invocations

--- reprocess-all-inputs.sh
+++ reprocess-all-inputs.sh
@@ -15,12 +15,12 @@ xcodebuild -project mtg.xcodeproj -scheme mtg-cli -configuration Release -derive
 ./build/Build/Products/Release/mtg-cli --add-to-collection collection/originals_from_tcgplayer/additions
 
 # decks
-./build/Build/Products/Release/mtg-cli --add=to-deck "10-22-23 wilds of eldraine draft" "collection/originals_from_tcgplayer/decks/10-22-23 wilds of eldraine draft deck.txt"
-./build/Build/Products/Release/mtg-cli --add=to-deck "10-27-23 wilds of eldraine draft" "collection/originals_from_tcgplayer/decks/10-27-23 wilds of eldraine draft deck.txt"
-./build/Build/Products/Release/mtg-cli --add=to-deck "10-31-23 dominaria remastered draft" "collection/originals_from_tcgplayer/decks/10-31-23 dominaria remastered draft deck (fixed).txt"
-./build/Build/Products/Release/mtg-cli --add=to-deck "10-31-23 dominaria remastered draft" "collection/originals_from_tcgplayer/decks/10-31-23 dominaria remastered draft deck (remainder).txt"
-./build/Build/Products/Release/mtg-cli --add=to-deck "1-5-2024 brothers war draft" "collection/originals_from_tcgplayer/decks/brothers war draft 1-5-2024.txt"
-./build/Build/Products/Release/mtg-cli --add=to-deck "goblins (red commander)" "collection/originals_from_tcgplayer/decks/goblin deck.txt"
-./build/Build/Products/Release/mtg-cli --add=to-deck "fae dominion (dimir commander)" "collection/originals_from_tcgplayer/decks/upgraded fae dominion.txt"
-./build/Build/Products/Release/mtg-cli --add=to-deck "sliver swarm (rainbow commander)" "collection/originals_from_tcgplayer/decks/upgraded sliver swarm.txt"
-./build/Build/Products/Release/mtg-cli --add=to-deck "veloci-ramp-tor (naya commander)" "collection/originals_from_tcgplayer/decks/upgraded veloci-ramp-tor.txt"
+./build/Build/Products/Release/mtg-cli --add-to-deck "10-22-23 wilds of eldraine draft" "collection/originals_from_tcgplayer/decks/10-22-23 wilds of eldraine draft deck.txt"
+./build/Build/Products/Release/mtg-cli --add-to-deck "10-27-23 wilds of eldraine draft" "collection/originals_from_tcgplayer/decks/10-27-23 wilds of eldraine draft deck.txt"
+./build/Build/Products/Release/mtg-cli --add-to-deck "10-31-23 dominaria remastered draft" "collection/originals_from_tcgplayer/decks/10-31-23 dominaria remastered draft deck (fixed).txt"
+./build/Build/Products/Release/mtg-cli --add-to-deck "10-31-23 dominaria remastered draft" "collection/originals_from_tcgplayer/decks/10-31-23 dominaria remastered draft deck (remainder).txt"
+./build/Build/Products/Release/mtg-cli --add-to-deck "1-5-2024 brothers war draft" "collection/originals_from_tcgplayer/decks/brothers war draft 1-5-2024.txt"
+./build/Build/Products/Release/mtg-cli --add-to-deck "goblins (red commander)" "collection/originals_from_tcgplayer/decks/goblin deck.txt"
+./build/Build/Products/Release/mtg-cli --add-to-deck "fae dominion (dimir commander)" "collection/originals_from_tcgplayer/decks/upgraded fae dominion.txt"
+./build/Build/Products/Release/mtg-cli --add-to-deck "sliver swarm (rainbow commander)" "collection/originals_from_tcgplayer/decks/upgraded sliver swarm.txt"
+./build/Build/Products/Release/mtg-cli --add-to-deck "veloci-ramp-tor (naya commander)" "collection/originals_from_tcgplayer/decks/upgraded veloci-ramp-tor.txt"
