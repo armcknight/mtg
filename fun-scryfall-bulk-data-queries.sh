9ba26106a9370d13ecfce2b22326c9838040c35a
Andrew McKnight <git@mcknight.rocks>
2024-01-28 18:20:31 -0900

    fix typo

--- fun-scryfall-bulk-data-queries.sh
+++ fun-scryfall-bulk-data-queries.sh
@@ -7,10 +7,10 @@
 #
 # Whenever I run an interesting jq query on the scryfall data dump, I record it here
 
-SRYFALL_DATA_DUMP_PATH="${1}"
+SCRYFALL_DATA_DUMP_PATH="${1}"
 
 # count the occurrences of keywords amongst cards, like flying, enchant, trample etc
-cat "${SRYFALL_DATA_DUMP_PATH}" | jq '.[] | .keywords | .[]' | sort | uniq -c | sort
+cat "${SCRYFALL_DATA_DUMP_PATH}" | jq '.[] | .keywords | .[]' | sort | uniq -c | sort
 
 # count the cards in each set present
-cat "${SRYFALL_DATA_DUMP_PATH}" | jq '.[] | .set_name' | sort | uniq -c | sort
+cat "${SCRYFALL_DATA_DUMP_PATH}" | jq '.[] | .set_name' | sort | uniq -c | sort
