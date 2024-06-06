5b39053ac0fd176ca291a7d5132de0649fc8a15a
Andrew McKnight <git@mcknight.rocks>
2024-06-05 19:24:33 -0800

    fixup! move TCGPlayerInfo to own code unit

--- mtg/TCGPlayerInfo.swift
+++ mtg/TCGPlayerInfo.swift
@@ -8,35 +8,13 @@
 import Foundation
 
 public struct TCGPlayerInfo {
-    /** Fields I get from the TCGPlayer scan app that are TCGPlayer specific data. */
-    public enum CSVHeader: String, CaseIterable {
-        case productID = "TCGPlayer Product ID"
-        case sku = "TCGPlayer SKU"
-        case priceEach = "TCGPlayer Price Each"
-        case fetchDate = "TCGPlayer Fetch Date"
-    }
-    
     var productID: Int?
     var SKU: String?
     var priceEach: Decimal?
     var fetchDate: Date?
-    
-    var csvRow: String {
-        [
-            "\(productID == nil ? "" : String(describing: productID!))",
-            "\(SKU ?? "")",
-            "\(priceEach == nil ? "" : String(describing: priceEach!))",
-            "\(fetchDate == nil ? "" : dateFormatter.string(from: fetchDate!))",
-        ].joined(separator: ",")
-    }
-    
-    init(productID: Int?, SKU: String? = nil, priceEach: Decimal? = nil, fetchDate: Date? = nil) {
-        self.productID = productID
-        self.SKU = SKU
-        self.priceEach = priceEach
-        self.fetchDate = fetchDate
-    }
-    
+}
+
+extension TCGPlayerInfo {
     init(managedCSVKeyValues keyValues: [String: String]) {
         var productID: Int?
         if let productIDString = keyValues[CSVHeader.productID.rawValue] {
@@ -55,5 +33,24 @@ public struct TCGPlayerInfo {
             fetchDate = dateFormatter.date(from: fetchDateString)
         }
         self = TCGPlayerInfo(productID: productID, SKU: sku, priceEach: priceEach, fetchDate: fetchDate)
+    }
+}
+
+extension TCGPlayerInfo {
+    /** Fields I get from the TCGPlayer scan app that are TCGPlayer specific data. */
+    public enum CSVHeader: String, CaseIterable {
+        case productID = "TCGPlayer Product ID"
+        case sku = "TCGPlayer SKU"
+        case priceEach = "TCGPlayer Price Each"
+        case fetchDate = "TCGPlayer Fetch Date"
+    }
+    
+    var csvRow: String {
+        [
+            "\(productID == nil ? "" : String(describing: productID!))",
+            "\(SKU ?? "")",
+            "\(priceEach == nil ? "" : String(describing: priceEach!))",
+            "\(fetchDate == nil ? "" : dateFormatter.string(from: fetchDate!))",
+        ].joined(separator: ",")
     }
 }
