4ab537423c50758e577a6cf257f155e119044ed3
Andrew McKnight <git@mcknight.rocks>
2025-01-03 16:09:56 -0900

    another interesting scryfall data query

--- fun-scryfall-bulk-data-queries.sh
+++ fun-scryfall-bulk-data-queries.sh
@@ -10,7 +10,10 @@
 SCRYFALL_DATA_DUMP_PATH="${1}"
 
 # count the occurrences of keywords amongst cards, like flying, enchant, trample etc
-cat "${SCRYFALL_DATA_DUMP_PATH}" | jq '.[] | .keywords | .[]' | sort | uniq -c | sort
+cat "${SCRYFALL_DATA_DUMP_PATH}" | jq '.[] | .keywords | .[]' | sort | uniq -c | sort > scryfall_oracle_keyword_occurrences.txt
 
 # count the cards in each set present
-cat "${SCRYFALL_DATA_DUMP_PATH}" | jq '.[] | .set_name' | sort | uniq -c | sort
+cat "${SCRYFALL_DATA_DUMP_PATH}" | jq '.[] | .set_name' | sort | uniq -c > scryfall_set_membership_totals.txt
+
+# count the number of cards at each rarity level in each set
+jq '.[] | .set + " " + .rarity' "${SCRYFALL_DATA_DUMP_PATH}" | sed 's/\"//g' | sort -t '\n' | uniq -c > scryfall_set_membership_by_rarity.txt
