69625b0343d844554b3778b2d944059f59f5ca87
Andrew McKnight <git@mcknight.rocks>
2025-04-20 23:17:11 -0800

    reprocess error check script fixes/improvements

--- scripts/check-reprocess-for-errors.sh
+++ scripts/check-reprocess-for-errors.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-grep -i -e trap -e error -e disagree -e fatal reprocess.log
+grep -i -e trap -e error -e disagree -e fatal reprocessed.log
+
+if [ $? -ne 0 ]; then
+    echo "No errors found."
+    exit 0
+else
+    exit 1
+fi
+
